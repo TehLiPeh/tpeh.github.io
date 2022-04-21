3f9cf928883c516ae9ade53d793eba81de70c89a
TehLiPeh <tpeh@uncc.edu>
2022-04-21 15:49:13 -0400

    Update arrays.js

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,4 +1,45 @@
-//Dropdown values
+var person = [];
+var salaries = [];
+
+function addSalary() {
+    let num = parseInt(document.getElementById("salary-value").value);
+
+    if (num >= 0) {
+        num = parseInt(num);
+        salaries.push(num);
+        document.getElementById("salary-value").value;
+        alert(num);
+    }
+    else {
+        alert("Please enter a valid salary value");
+    }
+}
+
+
+function displayResult() {
+    //Find the average
+    var avg = 0;
+    var sum = 0;
+    for (i = 0; i < salaries.length; i++) {
+        sum += salaries[i];
+    }
+    avg = sum / salaries.length;
+
+    //Find the Max 
+    const max = Math.max();
+    let result = "The max score is " + avg;
+    for (i = 0; i < salaries.length; i++) {
+        if (avg == max) {
+            document.getElementById("max-result").innerHTML = result;
+        }
+    }
+}
+
+function displaySalary() {
+
+}
+
+//Create dropdown
 function getEmployees() {
     var select = document.getElementById("dropdown");
     var options = [
@@ -6,22 +47,11 @@ function getEmployees() {
     ];
     
     for (var i = 0; i < options.length; i++) {
-        var val = options[i];
-        var element = document.createElement("option");
-        element.textContent = val;
-        element.value = val;
-        select.appendChild(element);
+        var names = options[i];
+        const createOption = document.createElement("option");
+        createOption.textContent = names;
+        createOption.names = names;
+        select.appendChild(createOption);
     }
 }
-
-function addSalary () {
-    var addedSal = "";
-
-}
-
-
-
-
-
-
 //https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array
